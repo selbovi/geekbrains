958da2c6c8265442f7a49badc62d0f97a5836ad4
selbovi <salavatm@gmail.com>
2019-07-17 22:30:34 +0300

    вторая часть за 5-ую домашку

---
+++ salavat-5/transactionForMovingData.sql
@@ -0,0 +1,48 @@
+DROP DATABASE IF EXISTS shop;
+CREATE  DATABASE shop CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+DROP DATABASE IF EXISTS sample;
+CREATE  DATABASE sample CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+DROP TABLE IF EXISTS shop.users;
+CREATE TABLE shop.users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) COMMENT 'Имя покупателя',
+  birthday_at DATE COMMENT 'Дата рождения',
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) COMMENT = 'Покупатели';
+
+DROP TABLE IF EXISTS sample.users;
+CREATE TABLE sample.users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) COMMENT 'Имя покупателя',
+  birthday_at DATE COMMENT 'Дата рождения',
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) COMMENT = 'Покупатели';
+
+INSERT INTO shop.users (name, birthday_at) VALUES
+  ('Геннадий', '1990-10-05'),
+  ('Наталья', '1984-11-12'),
+  ('Александр', '1985-05-20'),
+  ('Сергей', '1988-02-14'),
+  ('Иван', '1998-01-12'),
+  ('Мария', '1992-08-29');
+
+START TRANSACTION;
+
+INSERT into
+	sample.users
+SELECT * from shop.users u where u.id = 1;
+
+COMMIT;
+
+SELECT * FROM sample.users;
+
+
+
+
+
+
+
---
+++ salavat-5/viewOfProductsWithCatalogs.sql
@@ -0,0 +1,62 @@
+DROP DATABASE IF EXISTS SALAVAT_HOMEWORK;
+CREATE  DATABASE SALAVAT_HOMEWORK CHARACTER SET utf8 COLLATE utf8_general_ci;
+use SALAVAT_HOMEWORK;
+
+DROP TABLE IF EXISTS catalogs;
+CREATE TABLE catalogs (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) COMMENT 'Название раздела',
+  UNIQUE unique_name(name(10))
+) COMMENT = 'Разделы интернет-магазина';
+
+INSERT INTO catalogs VALUES
+  (NULL, 'Процессоры'),
+  (NULL, 'Материнские платы'),
+  (NULL, 'Видеокарты'),
+  (NULL, 'Жесткие диски'),
+  (NULL, 'Оперативная память');
+
+DROP TABLE IF EXISTS products;
+CREATE TABLE products (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) COMMENT 'Название',
+  description TEXT COMMENT 'Описание',
+  price DECIMAL (11,2) COMMENT 'Цена',
+  catalog_id INT UNSIGNED,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  KEY index_of_catalog_id (catalog_id)
+) COMMENT = 'Товарные позиции';
+
+INSERT INTO products
+  (name, description, price, catalog_id)
+VALUES
+  ('Intel Core i3-8100', 'Процессор для настольных персональных компьютеров, основанных на платформе Intel.', 7890.00, 1),
+  ('Intel Core i5-7400', 'Процессор для настольных персональных компьютеров, основанных на платформе Intel.', 12700.00, 1),
+  ('AMD FX-8320E', 'Процессор для настольных персональных компьютеров, основанных на платформе AMD.', 4780.00, 1),
+  ('AMD FX-8320', 'Процессор для настольных персональных компьютеров, основанных на платформе AMD.', 7120.00, 1),
+  ('ASUS ROG MAXIMUS X HERO', 'Материнская плата ASUS ROG MAXIMUS X HERO, Z370, Socket 1151-V2, DDR4, ATX', 19310.00, 2),
+  ('Gigabyte H310M S2H', 'Материнская плата Gigabyte H310M S2H, H310, Socket 1151-V2, DDR4, mATX', 4790.00, 2),
+  ('MSI B250M GAMING PRO', 'Материнская плата MSI B250M GAMING PRO, B250, Socket 1151, DDR4, mATX', 5060.00, 2);
+
+CREATE OR REPLACE VIEW
+	products_witw_catalogs
+as
+	SELECT
+		p.id, p.name as 'product', c.name as 'catalog'
+	from
+		products p
+	LEFT JOIN
+		catalogs c on p.catalog_id = c.id;
+;
+
+
+
+SELECT * FROM products_witw_catalogs;
+
+
+
+
+
+
+
